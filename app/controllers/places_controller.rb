1b14d2bb7e50395ca5dafe265696d53567f18c40
Gabriel Deluzuriaga <gabrieldeluzuriaga@gmail.com>
2018-08-27 15:14:42 -0400

    allow users to delte places created

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,4 +1,5 @@
 class PlacesController < ApplicationController
+  before_action :set_place, only: [:show, :destroy]
  
   def create
     @place = Place.new(place_params)
@@ -40,10 +41,18 @@ class PlacesController < ApplicationController
   end
  
   def show
-    @place = Place.find(params[:id])
+  end
+ 
+  def destroy
+    @place.destroy
+    redirect_to places_path, notice: "#{@place.name} deleted!"
   end
  
   private
+ 
+  def set_place
+    @place = Place.find(params[:id])
+  end
  
   def place_params
     params.require(:place).permit(:name, :street, :city, :state, :country, :latitude, :longitude)
